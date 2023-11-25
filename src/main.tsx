a0d988ea7f4b7ec52e5f19745036a5e659409cd7
Akash Agarwal <akash191095@gmail.com>
2023-11-25 18:43:47 +0530

    chore: remove boilerplate code

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,3 @@
-import "./index.css";
-
 import App from "./App.tsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
@@ -7,5 +5,5 @@ import ReactDOM from "react-dom/client";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
