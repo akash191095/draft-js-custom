fab8520381969712d14d71ce63fb61ea959dca4d
Akash Agarwal <akash191095@gmail.com>
2023-11-27 22:53:15 +0530

    feat(editor): add ability to save current state to localhost

--- src/components/Editor/CustomEditor.tsx
+++ src/components/Editor/CustomEditor.tsx
@@ -1,5 +1,6 @@
 import "draft-js/dist/Draft.css";
 
+import { convertFromRaw, convertToRaw } from "draft-js";
 import { useEffect, useState } from "react";
 
 import Editor from "@draft-js-plugins/editor";
@@ -16,26 +17,45 @@ const styleMap = {
 };
 
 function CustomEditor() {
-  const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty()
-  );
+  const [editorState, setEditorState] = useState(() => {
+    let initialEditorState = null;
+    const savedData = localStorage.getItem("draftRaw");
+    if (savedData) {
+      const rawContentFromStore = convertFromRaw(JSON.parse(savedData));
+      initialEditorState = EditorState.createWithContent(rawContentFromStore);
+    } else {
+      initialEditorState = EditorState.createEmpty();
+    }
+
+    return initialEditorState;
+  });
   const [editor, setEditor] = useState<Editor | null>(null);
 
   useEffect(() => {
     editor?.focus();
   }, [editor]);
 
+  function saveToLocalhost() {
+    const contentRaw = convertToRaw(editorState.getCurrentContent());
+    localStorage.setItem("draftRaw", JSON.stringify(contentRaw));
+  }
+
   return (
-    <div className={styles.container}>
-      <Editor
-        ref={(editor) => setEditor(editor)}
-        editorState={editorState}
-        onChange={setEditorState}
-        placeholder="Start writing..."
-        plugins={plugins}
-        customStyleMap={styleMap}
-      />
-    </div>
+    <>
+      <button className={styles.saveButton} onClick={saveToLocalhost}>
+        Save
+      </button>
+      <div className={styles.container}>
+        <Editor
+          ref={(editor) => setEditor(editor)}
+          editorState={editorState}
+          onChange={setEditorState}
+          placeholder="Start writing..."
+          plugins={plugins}
+          customStyleMap={styleMap}
+        />
+      </div>
+    </>
   );
 }
 
